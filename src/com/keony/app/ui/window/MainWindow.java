b3e5ec465b34b7bf38b173826c481953bdd73844
keon-y <keonyvictor@gmail.com>
2026-02-20 10:54:05 -0400

    chore: changed MainWindow to inherit JFrame

--- src/com/keony/app/ui/window/MainWindow.java
+++ src/com/keony/app/ui/window/MainWindow.java
@@ -3,25 +3,20 @@ package com.keony.app.ui.window;
 import com.keony.app.ui.components.GamePanel;
 import javax.swing.*;
 
-public class MainWindow {
+public class MainWindow extends JFrame {
 
-    private final JFrame window;
     private final GamePanel gamePanel;
 
     public MainWindow(int height, int width){
-        window = new JFrame();
         gamePanel = new GamePanel();
-        window.add(gamePanel);
-        window.setSize(width, height);
+        this.add(gamePanel);
+        this.setSize(width, height);
     }
 
     public GamePanel getGamePanel() {
         return this.gamePanel;
     }
 
-    public void setVisible(boolean visibility) {
-        this.window.setVisible(visibility);
-    }
-
+    
     
 }
