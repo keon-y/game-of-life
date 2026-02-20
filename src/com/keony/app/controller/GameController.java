c956ec450756308f00398351e8fc7b5bf5153c24
keon-y <keonyvictor@gmail.com>
2026-02-20 11:35:08 -0400

    chore: added mouseListener to controller

--- src/com/keony/app/controller/GameController.java
+++ src/com/keony/app/controller/GameController.java
@@ -2,8 +2,10 @@ package com.keony.app.controller;
 
 import com.keony.app.core.engine.GameEngine;
 import com.keony.app.ui.window.MainWindow;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class GameController {
+public class GameController implements MouseListener {
     private final MainWindow mainWindow;
     private final GameEngine gameEngine;
 
@@ -11,5 +13,31 @@ public class GameController {
         this.mainWindow = mainWindow;
         this.gameEngine = gameEngine;
         gameEngine.addListener(mainWindow.getGamePanel());
+        this.mainWindow.addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        //TODO: ADD MOUSE EVENTS
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //TODO: ADD MOUSE EVENTS
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //TODO: ADD MOUSE EVENTS
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //TODO: ADD MOUSE EVENTS
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //TODO: ADD MOUSE EVENTS
     }
 }
