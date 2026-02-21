7c81c68dbc671a79d5133391bcdd388ae1c91571
keon-y <keonyvictor@gmail.com>
2026-02-21 08:51:20 -0400

    feat: mouse drag to move screen

--- src/com/keony/app/controller/GameController.java
+++ src/com/keony/app/controller/GameController.java
@@ -2,14 +2,18 @@ package com.keony.app.controller;
 
 import com.keony.app.core.engine.GameEngine;
 import com.keony.app.ui.window.MainWindow;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import javax.swing.JViewport;
+import javax.swing.SwingUtilities;
 
 public class GameController implements MouseListener, MouseMotionListener {
     private final MainWindow mainWindow;
     private final GameEngine gameEngine;
     private boolean firstCellState; // Saves the state of a cell before clicking
+    private Point lastMousePosition;
 
 
     public GameController(MainWindow mainWindow, GameEngine gameEngine) {
@@ -26,6 +30,11 @@ public class GameController implements MouseListener, MouseMotionListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
+        if (SwingUtilities.isRightMouseButton(e)) { // Drag and move
+            lastMousePosition = e.getLocationOnScreen();
+            return;
+        } 
+        
         int cellSize = mainWindow.getGamePanel().getCellsize();
         this.firstCellState = gameEngine.getCell(e.getX() / cellSize, e.getY() / cellSize); // Captures the state of the first clicked cell
         gameEngine.toggleCell(e.getX() / cellSize, e.getY() / cellSize);
@@ -33,8 +42,34 @@ public class GameController implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        // Drag and move
+        if (SwingUtilities.isRightMouseButton(e)) {
+            
+            if (lastMousePosition != null) {
+                Point currentMousePosition = e.getLocationOnScreen();
+                
+                int deltaX = lastMousePosition.x - currentMousePosition.x;
+                int deltaY = lastMousePosition.y - currentMousePosition.y;
+
+                JViewport viewport = (JViewport) SwingUtilities.getAncestorOfClass(JViewport.class, mainWindow.getGamePanel());
+                
+                if (viewport != null) {
+                    Point viewPos = viewport.getViewPosition();
+                    viewPos.translate(deltaX, deltaY);
+                    viewport.setViewPosition(viewPos);
+                }
+                
+                lastMousePosition = currentMousePosition;
+            }
+            return;
+
+        }
+        
+        // Paint cells
         int cellSize = mainWindow.getGamePanel().getCellsize();
         gameEngine.setCell(e.getX() / cellSize, e.getY() / cellSize, !firstCellState);
+        
+
     }
 
     @Override
--- src/com/keony/app/ui/window/MainWindow.java
+++ src/com/keony/app/ui/window/MainWindow.java
@@ -1,6 +1,7 @@
 package com.keony.app.ui.window;
 
 import com.keony.app.ui.components.GamePanel;
+import java.awt.Dimension;
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
@@ -8,15 +9,26 @@ public class MainWindow extends JFrame {
     private final GamePanel gamePanel;
 
     public MainWindow(int height, int width){
-        gamePanel = new GamePanel();
-        this.add(gamePanel);
+        this.gamePanel = new GamePanel();
+        this.gamePanel.setPreferredSize(new Dimension(2000, 2000));
         this.setSize(width, height);
+
+        JScrollPane scrollPane = new JScrollPane(gamePanel);
+        
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+
+        this.add(scrollPane);
+        
     }
 
     public GamePanel getGamePanel() {
         return this.gamePanel;
     }
 
+    public void setGameBounds(int x, int y) {
+        gamePanel.setBounds(x, y, this.getWidth(), this.getHeight());
+    }
     
     
 }
